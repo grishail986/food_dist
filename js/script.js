30d9ced5ab4798502900ddd1506c78697c9fec5d
Gregory Ilchenko <grishail986@gmail.com>
2023-04-30 14:23:52 +0400

    feat: add timer and update readme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    // Tabs  
+    // Tabs: 
 
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
             elem.style.display = 'none'
         })
 
-        tabs.forEach(elem => {                                  // убираем активность у элементов 
+        tabs.forEach(elem => {                              // убираем активность у элементов 
             elem.classList.remove('tabheader__item_active')
         })
     }
@@ -38,4 +38,67 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     
+    // Timer: 
+
+    let deadline = '2023-05-06'
+
+    function getTimeRemaining(endtime) {                        // разница между дедлайном и текущем временем, в качестве аргумента используем deadline 
+        let days, hours, minutes, seconds
+        let t = Date.parse(endtime) - Date.parse(new Date())    // количество мс до дедлайна - текущая дата в мс = разница, далее эту разницу в мс нужно конвертировать в количество дней, часов, минут и секунд 
+            
+        if (t <= 0) {
+            days = 0
+            hours = 0
+            minutes = 0
+            seconds = 0
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),       // считаем дни: разницу делим на количество мс в одном дне и округляем 
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),    // часы: разницу делим на количество мс в одном часе, вычисляем остаток от деления на 24 и округляем 
+            minutes = Math.floor((t / 1000 / 60) % 60),         // минуты: остаток от деления на 60 
+            seconds = Math.floor((t / 1000) % 60)               // секунды 
+        }
+
+        return {                        // возвращаем объект с переменными, чтобы их использовать дальше 
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+    
+    function getZero(num) {
+        if (num >= 0 && num < 10) {                     // если число меньше десяти, добавляем перед ним 0 
+            return `0${num}`
+        } else {
+            return num
+        }
+    }
+
+    function setClock(selector, endtime) {                      // настраиваем часы 
+        let timer = document.querySelector(selector),           // ищем класс 'timer' на странице, см арументы при вызове 
+            days = timer.querySelector('#days'),                // ищем элемент days, обращаясь к timer 
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000)       // запускаем updateClock каждую секунду 
+        
+        updateClock()
+        
+        function updateClock() {                        // обновляем таймер 
+            let t = getTimeRemaining(endtime)           // вызываем getTimeRemaining и возвращаем объект с данными 
+            
+            days.innerHTML = getZero(t.days)            // меняем текущее значение на значение из свойства days, обращаясь к объекту t  
+            hours.innerHTML = getZero(t.hours)
+            minutes.innerHTML = getZero(t.minutes)
+            seconds.innerHTML = getZero(t.seconds)
+            
+            if (t.total <= 0) {                         // если значение из объекта <= 0, отменяем setInterval 
+                clearInterval(timeInterval)
+            }
+        }
+    }
+
+    setClock('.timer', deadline)
+
 })
